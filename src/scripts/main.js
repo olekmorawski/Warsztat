427fc9feacec6c188d27052c21939348f0a5cc37
Aleksander Morawski <olek.morawski@gmail.com>
2023-07-21 17:17:16 +0200

    Wreszcie cos dziala c:

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,47 @@
+function updateSummary(selectedPackage) {
+  const packageItem = document.querySelector(
+    ".list__item[data-id='package'] .item__calc"
+  );
+  const totalPrice = document.querySelector(".total__price");
+
+  if (selectedPackage === "basic") {
+    packageItem.textContent = "Basic";
+  } else if (selectedPackage === "professional") {
+    packageItem.textContent = "Professional";
+  } else if (selectedPackage === "premium") {
+    packageItem.textContent = "Premium";
+  }
+
+  // Przykład:
+  const productsValue = parseInt(productsInput.value) || 0;
+  const ordersValue = parseInt(ordersInput.value) || 0;
+  const productsPrice = productsValue * 0.5;
+  const ordersPrice = ordersValue * 0.5;
+
+  updateSummaryLine("products", `${productsValue} * $0.5`, `$${productsPrice}`);
+  updateSummaryLine("orders", `${ordersValue} * $0.5`, `$${ordersPrice}`);
+
+  const totalPriceValue =
+    productsPrice +
+    ordersPrice +
+    packagePrice +
+    accountingPrice +
+    terminalPrice;
+  totalPrice.textContent = `$${totalPriceValue}`;
+}
+
+function updateSummaryLine(id, calculation, price) {
+  const line = document.querySelector(`.list__item[data-id="${id}"]`);
+  line.style.display = "block";
+  line.querySelector(".item__calc").textContent = calculation;
+  line.querySelector(".item__price").textContent = price;
+}
+
+function hideSummaryLine(id) {
+  const line = document.querySelector(`.list__item[data-id="${id}"]`);
+  line.style.display = "none";
+}
+
 const selectorInputRef = document.querySelector(".select__input");
 const selectDropdownRef = document.querySelector(".select__dropdown");
 const listItemRefs = document.querySelectorAll(".select__dropdown li");
@@ -18,17 +62,23 @@ selectorInputRef.addEventListener("click", () => {
   });
 });
 
-function updateSummary(selectedPackage) {
-  const packageItem = document.querySelector(
-    ".list__item[data-id='package'] .item__calc"
-  );
-  const totalPrice = document.querySelector(".total__price");
+const productsInput = document.querySelector(".form__input#products");
+const ordersInput = document.querySelector(".form__input#orders");
 
-  if (selectedPackage === "basic") {
-    packageItem.textContent = "Basic";
-  } else if (selectedPackage === "professional") {
-    packageItem.textContent = "Professional";
-  } else if (selectedPackage === "premium") {
-    packageItem.textContent = "Premium";
+productsInput.addEventListener("input", () => {
+  const value = parseInt(productsInput.value);
+  if (Number.isInteger(value) && value > 0) {
+    updateSummary();
+  } else {
+    hideSummaryLine("products");
   }
-}
+});
+
+ordersInput.addEventListener("input", () => {
+  const value = parseInt(ordersInput.value);
+  if (Number.isInteger(value) && value > 0) {
+    updateSummary();
+  } else {
+    hideSummaryLine("orders");
+  }
+});
