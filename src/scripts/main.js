22f8f7e3e2830be9158c9931c5f7e225326e3d63
Aleksander Morawski <olek.morawski@gmail.com>
2023-07-21 18:48:55 +0200

    v2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,16 @@
+let selectedPackage = "";
+let packagePrice = 0;
+let accountingPrice = 0;
+let terminalPrice = 0;
+
+const selectorInputRef = document.querySelector(".select__input");
+const selectDropdownRef = document.querySelector(".select__dropdown");
+const listItemRefs = document.querySelectorAll(".select__dropdown li");
+const productsInput = document.querySelector(".form__input#products");
+const ordersInput = document.querySelector(".form__input#orders");
+const packageInput = document.querySelector("#packageValue");
+const packageWindow = document.querySelector("#packageWindow");
+
 function updateSummary(selectedPackage) {
   const packageItem = document.querySelector(
     ".list__item[data-id='package'] .item__calc"
@@ -16,10 +29,10 @@ function updateSummary(selectedPackage) {
   const productsValue = parseInt(productsInput.value) || 0;
   const ordersValue = parseInt(ordersInput.value) || 0;
   const productsPrice = productsValue * 0.5;
-  const ordersPrice = ordersValue * 0.5;
+  const ordersPrice = ordersValue * 0.25;
 
   updateSummaryLine("products", `${productsValue} * $0.5`, `$${productsPrice}`);
-  updateSummaryLine("orders", `${ordersValue} * $0.5`, `$${ordersPrice}`);
+  updateSummaryLine("orders", `${ordersValue} * $0.25`, `$${ordersPrice}`);
 
   const totalPriceValue =
     productsPrice +
@@ -32,7 +45,7 @@ function updateSummary(selectedPackage) {
 
 function updateSummaryLine(id, calculation, price) {
   const line = document.querySelector(`.list__item[data-id="${id}"]`);
-  line.style.display = "block";
+  line.style.display = "flex";
   line.querySelector(".item__calc").textContent = calculation;
   line.querySelector(".item__price").textContent = price;
 }
@@ -41,10 +54,6 @@ function hideSummaryLine(id) {
   const line = document.querySelector(`.list__item[data-id="${id}"]`);
   line.style.display = "none";
 }
-
-const selectorInputRef = document.querySelector(".select__input");
-const selectDropdownRef = document.querySelector(".select__dropdown");
-const listItemRefs = document.querySelectorAll(".select__dropdown li");
 
 selectorInputRef.addEventListener("click", () => {
   selectDropdownRef.style.display = "block";
@@ -57,13 +66,18 @@ selectorInputRef.addEventListener("click", () => {
       selectorInputRef.textContent =
         selectedValue.charAt(0).toUpperCase() + selectedValue.slice(1);
 
+      if (selectedPackage === "basic" || selectedPackage === "professional") {
+        hidePackageWindow();
+        packagePrice = 0;
+      } else if (selectedPackage === "premium") {
+        showPackageWindow();
+        packagePrice = 60;
+      }
+
       updateSummary(selectedValue);
     });
   });
 });
-
-const productsInput = document.querySelector(".form__input#products");
-const ordersInput = document.querySelector(".form__input#orders");
 
 productsInput.addEventListener("input", () => {
   const value = parseInt(productsInput.value);
@@ -82,3 +96,13 @@ ordersInput.addEventListener("input", () => {
     hideSummaryLine("orders");
   }
 });
+
+function showPackageWindow() {
+  packageWindow.style.display = "block";
+}
+
+function hidePackageWindow() {
+  packageWindow.style.display = "none";
+}
+
+hidePackageWindow();
